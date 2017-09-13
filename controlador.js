a06c3313207c24b3e55dcfe1a9eac68a75628398
dfhs1994 <dfhs1994@gmail.com>
2017-09-13 12:45:56 -0300

    avance practica 13-09
    manejo de estilos funciones,

--- controlador.js
+++ controlador.js
@@ -1,63 +1,65 @@
-(function(){
-  angular
-  .module('directivas', [])
-
-  .directive('usuario', function() {
-    var valoresInternos = {};
-
-    valoresInternos.scope={
-      nombreUsuario : '@nombre',
-      apellidoUsuario : '@apellido'
-    }
-      valoresInternos.template = "¡Bienvenido {{nombreUsuario}} {{apellidoUsuario}}!";
-
-    return valoresInternos;
-  });
-
-
-})();
+// (function(){
+//   angular
+//   .module('directivas', [])
+//
+//   .directive('usuario', function() {
+//     var valoresInternos = {};
+//
+//     valoresInternos.scope={
+//       nombreUsuario : '@nombre',
+//       apellidoUsuario : '@apellido'
+//     }
+//       valoresInternos.template = "¡Bienvenido {{nombreUsuario}} {{apellidoUsuario}}!";
+//
+//     return valoresInternos;
+//   });
+//
+//
+// })();
 
 (function () {
 
-  var misEstilos =angular
+  angular
+  .module('estilos', [])
 
-  .module('estilosApp', []);
-  misEstilos
+  .controller('controladorEstilos', ["$scope", function ($scope){
 
-  .controller('controladorEstilos', function ($scope){
-
-  $scope.opcionA = function () {
-
-  }
-
-  $scope.opcionB = function () {
-
-  }
-  $scope.cambiarEstilo = function () {
-  $scope.estilosAngular = {
- width:'600px',
- height:'200px',
- borderRadius:'10px'
- }
-
-  }
-
-  $scope.cambiarClase = function () {
-    $scope.clasesAngular = 'segundoEstilo'
-
-  }
-
-  $scope.ocultarCaja = function () {
-
-  $scope.ocultarMenu = true
-
-  }
-  $scope.mostrarCaja = function () {
-    $scope.ocultarMenu = false
-
-  }
-
-  })
+    $scope.imagenActiva = 'http://jorgesanchezmartinez.com/wp-content/uploads/2017/02/aceptar_incertidumbre_jorge_sanchez_martinez.png'
 
 
-});
+
+    $scope.opcionA = function () {
+      $scope.imagenActiva = 'https://www.callofduty.com/content/dam/atvi/callofduty/hub/main-hub/iw-hub/common/nav/aw-art.jpg'
+
+    }
+
+    $scope.opcionB = function () {
+      $scope.imagenActiva ='https://store.playstation.com/store/api/chihiro/00_09_000/container/ID/en/999/UP0002-CUSA03005_00-ASIACODBO3GAME01/1502366730000/image?_version=00_09_000&platform=chihiro&w=225&h=225&bg_color=000000&opacity=100'
+
+    }
+
+    $scope.cambiarEstilo = function () {
+      $scope.estilosAngular = {
+         width:'600px',
+         height:'200px',
+         borderRadius:'10px'
+       }
+    }
+
+    $scope.cambiarClase = function () {
+      $scope.clasesAngular = 'segundoEstilo'
+    }
+
+    $scope.ocultarCaja = function () {
+
+      $scope.ocultar_menu = true;
+    }
+
+    $scope.mostrarCaja = function () {
+      $scope.ocultar_menu = false
+    }
+
+  }]);
+
+
+})();
